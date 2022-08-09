35fec1a8f5c1a5a9a8b679b5187a22c567cccea4
unknown <vitagrebennik@gmail.com>
2022-08-09 13:46:45 +0300

    add jimp handler

--- controllers/users/setAvatar.js
+++ controllers/users/setAvatar.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { basedir } = global;
 
@@ -8,23 +9,24 @@ const { User } = require(`${basedir}/models/users`);
 const avatarsDir = path.join(basedir, "public", "avatars");
 
 const setAvatar = async (req, res) => {
+  // Беремо шлях і ім"я аватарки, де вона зараз знаходиться
+  const { path: tempPath, originalname } = req.file;
+  // Беремо _id юзера з req.users
+  const { _id } = req.user;
+  // Робимо унікальне ім"я
+  const imageName = `${_id}_${originalname}`;
+  // Оброблюємо пакетом Jimp
+  const avatar = await Jimp.read(tempPath);
+  await avatar.resize(250, 250).write(tempPath);
+
   try {
-    // Беремо _id юзера з req.users
-    const { _id } = req.user;
-    // Беремо шлях і ім"я аватарки, де вона зараз знаходиться
-    const { path: tempPath, originalname } = req.file;
-
-    // Робимо унікальне ім"я
-    const [extention] = originalname.split(".").reverse();
-    const newName = `${_id}.${extention}`;
-
     // Створюємо новий шлях аватарки, де вона буде знаходитись
-    const uploudPath = path.join(avatarsDir, newName);
+    const uploudPath = path.join(avatarsDir, imageName);
     // Переміщуємо її
     await fs.rename(tempPath, uploudPath);
 
     // Запам"ятовуємо цей шлях, записуємо в базу
-    const avatarURL = path.join("avatars", newName);
+    const avatarURL = path.join("avatars", imageName);
     await User.findByIdAndUpdate(_id, { avatarURL });
 
     // Повертаємо цей шлях до аватарки
@@ -32,7 +34,7 @@ const setAvatar = async (req, res) => {
       avatarURL,
     });
   } catch (error) {
-    await fs.unlink(req.file.path);
+    await fs.unlink(tempPath);
     throw error;
   }
 };
